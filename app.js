985e653fd76d672eedf1d3a7ff8aa0e803df0f2a
kaxadeveloper <kaxa3developer@yahoo.com>
2025-04-30 07:13:53 +0400

    app

---
+++ app.js
@@ -0,0 +1,39 @@
+const testimonials = [
+    {
+        name: "Cherise G",
+        photoUrl: "https://images.unsplash.com/photo-1560250097-0b93528c311a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        text: "This is simply unbelievable! I would be lost without Apple. The very best. Not able to tell you how happy I am with Apple."
+    },
+    {
+        name: "Rosetta Q",
+        photoUrl: "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        text: "I would also like to say thank you to all your staff. Wow what great service, I love it! Apple impressed me on multiple levels."
+    },
+    {
+        name: "Constantine V",
+        photoUrl: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        text: "Thank you for making it painless, pleasant and most of all hassle free! I wish I would have thought of it first. The very best.",
+    },
+];
+
+const imgEl = document.querySelector("img");
+const textEl = document.querySelector(".text");
+const usernameEl = document.querySelector(".username");
+
+let idx = 0;
+
+updateTestimonial()
+
+function updateTestimonial() {
+    const {name, photoUrl, text} = testimonials[idx];
+    imgEl.src = photoUrl;
+    textEl.innerText = text;
+    usernameEl.innerText = name;
+    idx++
+    if(idx === testimonials.length){
+        idx = 0;
+    }
+    setTimeout(()=> {
+      updateTestimonial()  
+    }, 10000)
+}
